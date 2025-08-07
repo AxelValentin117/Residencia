26e5acbbf57d3d0a71822435bbe319e055479849
Axel Valenitn <axelvalentin34@gmail.com>
2025-08-07 09:21:43 -0600

    modificacion

--- resources/views/cursos/edit.blade.php
+++ resources/views/cursos/edit.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.plantilla')
 @section('title','cursos edit')
 @section('content')
-    <h1>En esta pagina podras editar un curso</h1>
+    <h1>En esta pagina podras editar un curso de esta version</h1>
     <form action="{{route('cursos.update', $curso)}}" method="post">
         @csrf
+        @method()
         <label>
             Nombre:
             <br>
