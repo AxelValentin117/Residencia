7eacb8a744344045f6a40ec23e34435de9d84234
Axel Valenitn <axelvalentin34@gmail.com>
2025-08-08 12:43:58 -0600

    a

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
 
 Route::get('/', HomeController::class);
-
+Route::get('cursos/cuestionarios',[CursoController::class, 'cuestionario'])->name('cursos.cuestionarios');
 Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas'=>'curso'])->names('cursos');
